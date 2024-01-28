0b50eb0a85b51487d2db01dc7ee1dbea0281547b
francisco1423mkt <franciscomaitre@outlook.com>
2024-01-28 10:31:06 -0300

    criei alua 014
    numeros

---
+++ index.js
@@ -0,0 +1,9 @@
+let num1 = 10.5789551255547;
+let num2 = 2.5;
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2)); // REPRESENTAÇÃO BINARIA DO NUMERO.
+//console.log(num1.toFixed(2)); // ARREDONDAMENTO EM CASAS DECIMAIS.
+
+
